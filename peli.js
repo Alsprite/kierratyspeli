105f473ce583bc618fcf2a0ca88d1376c460d612
Alsprite <henri.mykra@esedulainen.fi>
2022-10-11 13:45:15 +0300

    a

--- peli.js
+++ peli.js
@@ -1,16 +1,14 @@
-function avaaOhjeet() {
-
+function ohjeet() {
+    let ohje = document.querySelector(".ohjeruutu");
+    if (ohje.style.display == "none") {
+        ohje.style.display = "block";
+        document.getElementById("ohjenappula").innerText = "Takaisin";
+    } else {
+        ohje.style.display = "none";
+        document.getElementById("ohjenappula").innerText = "Ohjeet";
+    }
 }
 function peliAloitus() {
     document.querySelector(".alkuruutu").style.display = "none";
     document.querySelector(".peliruutu").style.display = "block";
-}
-function ohjeet() {
-    if (document.querySelector(".ohjeruutu").style.display == "none") {
-        document.querySelector(".ohjeruutu").style.display = "block";
-        document.getElementById("ohjenappula").innerText = "Takaisin";
-    } else {
-        document.querySelector(".ohjeruutu").style.display = "none";
-        document.getElementById("ohjenappula").innerText = "Ohjeet";
-    }
 }
