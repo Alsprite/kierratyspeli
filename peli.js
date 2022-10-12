13b28fb2cc8702f95a512fa0c4dbf64bf5a53ee9
Alsprite <henri.mykra@esedulainen.fi>
2022-10-12 12:04:18 +0300

    a

--- peli.js
+++ peli.js
@@ -2,29 +2,29 @@ let kieli;
 var eramaara = 1;
 
 var astia1 = new Image()
-astia1.src="kuvat/1metalli.jpg";
+astia1.src="kuvat/1metalli.png";
 var astia2 = new Image()
-astia2.src="kuvat/2vaarallinen.jpg";
+astia2.src="kuvat/2vaarallinen.png";
 astia3 = new Image()
-astia3.src="kuvat/3kartonki.jpg";
+astia3.src="kuvat/3kartonki.png";
 var astia4 = new Image()
-astia4.src="kuvat/4paperi.jpg";
+astia4.src="kuvat/4paperi.png";
 var astia5 = new Image()
-astia5.src="kuvat/5seka.jpg";
+astia5.src="kuvat/5seka.png";
 var astia6 = new Image()
-astia6.src="kuvat/6muovi.jpg";
+astia6.src="kuvat/6muovi.png";
 var astia7 = new Image()
-astia7.src="kuvat/7lasi.jpg";
+astia7.src="kuvat/7lasi.png";
 var astia8 = new Image()
-astia8.src="kuvat/8energia.jpg";
+astia8.src="kuvat/8energia.png";
 var astia9 = new Image()
-astia9.src="kuvat/9bio.jpg";
+astia9.src="kuvat/9bio.png";
 var astia10 = new Image()
-astia10.src="kuvat/10ser.jpg";
+astia10.src="kuvat/10ser.png";
 var astia11 = new Image()
-astia11.src="kuvat/11pullonpalautus.jpg";
+astia11.src="kuvat/11pullonpalautus.png";
 var astia12 = new Image()
-astia12.src="kuvat/12paristo.jpg";
+astia12.src="kuvat/12paristo.png";
 
 function ohjeet() {
     let ohje = document.querySelector(".ohjeruutu");
