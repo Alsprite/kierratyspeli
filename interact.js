39371f7cbf7624f4a5c7df169e68dbba0bfd7398
Alsprite <henri.mykra@esedulainen.fi>
2022-10-31 09:31:18 +0200

    drag and drop parannust

--- interact.js
+++ interact.js
@@ -1,20 +1,59 @@
-interact('.draggable')
+interact('.dropzone').dropzone({
+  // only accept elements matching this CSS selector
+  accept: '#dropattava',
+  // Require a 75% element overlap for a drop to be possible
+  overlap: 0.75,
+
+  // listen for drop related events:
+
+  ondropactivate: function (event) {
+    // add active dropzone feedback
+    event.target.classList.add('drop-active')
+  },
+  ondragenter: function (event) {
+    var draggableElement = event.relatedTarget
+    var dropzoneElement = event.target
+
+    // feedback the possibility of a drop
+    dropzoneElement.classList.add('drop-target')
+    draggableElement.classList.add('can-drop')
+    draggableElement.textContent = 'Dragged in'
+  },
+  ondragleave: function (event) {
+    // remove the drop feedback style
+    event.target.classList.remove('drop-target')
+    event.relatedTarget.classList.remove('can-drop')
+    event.relatedTarget.textContent = 'Dragged out'
+  },
+  ondrop: function (event) {
+    event.relatedTarget.textContent = 'Dropped'
+  },
+  ondropdeactivate: function (event) {
+    // remove active dropzone feedback
+    event.target.classList.remove('drop-active')
+    event.target.classList.remove('drop-target')
+  }
+})
+
+  interact('.draggable')
   .draggable({
-    // enable inertial throwing
     inertia: true,
-    // keep the element within the area of it's parent
     modifiers: [
       interact.modifiers.restrictRect({
         restriction: 'parent',
         endOnly: true
       })
     ],
-    // enable autoScroll
     autoScroll: true,
 
     listeners: {
       // call this function on every dragmove event
       move: dragMoveListener,
+
+      // call this function on every dragend event
+      end (event) {
+        
+      }
     }
   })
 
@@ -31,58 +70,4 @@ function dragMoveListener (event) {
   target.setAttribute('data-x', x)
   target.setAttribute('data-y', y)
 }
-
-// this function is used later in the resizing and gesture demos
 window.dragMoveListener = dragMoveListener
-
-interact('#oikeeDropzone').dropzone({
-    // only accept elements matching this CSS selector
-    accept: '#dropattava',
-    // Require a 75% element overlap for a drop to be possible
-    overlap: 0.75,
-  
-    // listen for drop related events:
-  
-    ondropactivate: function (event) {
-      // add active dropzone feedback
-      event.target.classList.add('drop-active')
-    },
-    ondragenter: function (event) {
-      var draggableElement = event.relatedTarget
-      var dropzoneElement = event.target
-  
-      // feedback the possibility of a drop
-      dropzoneElement.classList.add('drop-target')
-      draggableElement.classList.add('can-drop')
-      // draggableElement.textContent = 'Dragged in'
-    },
-    ondragleave: function (event) {
-      // remove the drop feedback style
-      event.target.classList.remove('drop-target')
-      event.relatedTarget.classList.remove('can-drop')
-      // event.relatedTarget.textContent = 'Dragged out'
-    },
-    ondrop: function (event) {
-      console.log("joo");
-      // event.relatedTarget.textContent = 'Dropped'
-    },
-    ondropdeactivate: function (event) {
-      // remove active dropzone feedback
-      event.target.classList.remove('drop-active')
-      event.target.classList.remove('drop-target')
-    }
-  })
-  
-  interact('.drag-drop')
-    .draggable({
-      inertia: true,
-      modifiers: [
-        interact.modifiers.restrictRect({
-          restriction: 'parent',
-          endOnly: true
-        })
-      ],
-      autoScroll: true,
-      // dragMoveListener from the dragging demo above
-      listeners: { move: dragMoveListener }
-    })
