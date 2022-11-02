8d6861d7b5253c1e92a02b8bebfb806e513e6f1f
juhr01 <juho1.riipinen@gmail.com>
2022-11-02 11:57:21 +0200

    push

--- interact.js
+++ interact.js
@@ -230,6 +230,7 @@ function randomizeTrashBins() {
   } while (trash.TrashBin != correctTrashBin && !usedTrashArray.includes(trash))
 
   document.querySelector("#dragP").textContent = trash.TrashItem;
+  document.querySelector('#dropattava').className = correctBinId;
 
   console.log(trash);
   usedTrashArray.push(trash)
@@ -239,6 +240,7 @@ function randomizeTrashBins() {
 function startGame() {
   document.querySelector(".alkuruutu").style.display = "none";
   document.querySelector(".peliruutu").style.display = "block";
+  hideBinLabels();
   getTrashArray();
 }
 
